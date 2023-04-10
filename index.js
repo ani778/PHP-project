d268330772af86a01e7438899e3a7dc369f57867
ani778 <ani778h@gmail.com>
2023-04-11 01:27:01 +0400

    Finel bug fixing

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
   const hamburger = document.querySelector(".hamburger");
   const navMenu = document.querySelector(".nav");
+  var acc = document.getElementsByClassName("accordion");
+  var i;
   
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
   hamburger.addEventListener("click", mobileMenu);
   
   function mobileMenu() {
